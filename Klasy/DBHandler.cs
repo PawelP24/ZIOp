183cc6e0a61bd9eb83cb51e4c1353d9d22556f8b
PawelP24 <pawep@vp.pl>
2017-05-25 11:31:38 +0200

    Add files via upload

---
+++ Klasy/DBHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_biblioteczny
+{
+    public class DBHandler
+    {
+        private string connectionString ;
+        SqlConnection connection;
+        public DBHandler()
+        {
+            this.connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Biblioteka; Integrated Security = True; Pooling = False";
+            connection = new SqlConnection(this.connectionString);
+            connection.Open();
+        }
+
+        public void Add_Pracownik()
+        {
+            SqlCommand command = connection.CreateCommand();
+            
+
+           
+        }
+    }
+}
---
+++ Klasy/Spis_pracownikow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace System_biblioteczny
+{
+    /// <summary>
+    /// Interaction logic for Spis_pracownikow.xaml
+    /// </summary>
+    public partial class Spis_pracownikow : Window
+    {
+        public Spis_pracownikow()
+        {
+            InitializeComponent();
+        }
+
+        private void B_dodaj_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
